b90103c0fcecefc1e4c218997ac7e556891c2907
Rahul Yadav <rahulyadav19.ry@gmail.com>
2025-06-17 22:05:05 +0530

    Ticket Lifecycle management

--- src/Ticket.java
+++ src/Ticket.java
@@ -4,13 +4,21 @@ public class Ticket {
     private final int mId;
     private Priority mPriority;
     private String mDescription;
+    private TicketState mState;
 
     Ticket(int id) {
         mId = id;
+        mState = TicketState.OPEN;
+        System.out.println("The ticket is created successfully and the state is: " + mState);
+    }
+    
+    public void setTicketState(TicketState state) {
+        mState = state;
+        System.out.println("The ticket state has been changed to "+ mState);
     }
 
     public void setDescription(String description) {
-        mDescription = mDescription;
+        mDescription = description;
     }
 
     public void setPriority(Priority priority) {
@@ -19,5 +27,9 @@ public class Ticket {
 
     public Priority getPriority() {
         return mPriority;
+    }
+
+    public TicketState getState() {
+        return mState;
     }
 }
---
+++ src/TicketState.java
@@ -0,0 +1,5 @@
+package src;
+
+public enum TicketState {
+    OPEN,IN_PROGRESS,RESOLVED,CLOSED
+}
